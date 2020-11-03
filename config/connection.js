58385de218525b6bb70d59cd7ab75853b502bc31
smarten <smarten.developer@gmail.com>
2020-11-02 20:28:01 -0800

    update DB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 let connection;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'fwitter_db',
-  }).promise();
+  connection = mysql
+    .createConnection({
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "hype_type_db",
+    })
+    .promise();
 }
 module.exports = connection;
