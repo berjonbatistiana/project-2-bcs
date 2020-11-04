0762d9c9730c9cd65779936021cf4cdd8c38e623
smarten <smarten.developer@gmail.com>
2020-11-03 21:10:11 -0800

    cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,18 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Navbar from './pages/common/components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./pages/common/components/Navbar";
 import { useSelector, useDispatch } from "react-redux";
-
-// import word from './utils/GenerateWord';
-import {
-  WrappedSignUp,
-  WrappedSignIn,
-  About,
-} from './pages/Viewer';
-
-import { Dashboard } from './pages/User'
-// const randomWord = word.generateWord(3, 5);
-
+import { Dashboard } from "./pages/User";
+import { WrappedSignUp, WrappedSignIn, About } from "./pages/Viewer";
 
 function App() {
   const { token } = useSelector((state) => state.viewer);
   return (
     <Router>
-      <Navbar/>
-      <Route path='/signup' component={WrappedSignUp}/>
-      <Route path='/signin' component={WrappedSignIn}/>
-      <Route exact path='/' component={token ? Dashboard : About}/>
+      <Navbar />
+      <Route path="/signup" component={WrappedSignUp} />
+      <Route path="/signin" component={WrappedSignIn} />
+      <Route exact path="/" component={token ? Dashboard : About} />
     </Router>
   );
 }
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken');
-const {
-  insertUserToDb
-} = require('../model/userOrm');
+const jwt = require("jsonwebtoken");
+const { insertUserToDb } = require("../model/userOrm");
 
 const tokenForUser = (id) => {
-  return jwt.sign({
-    sub: id,
-    iat: new Date().getTime()
-  }, process.env.JWT_SECRET);
+  return jwt.sign(
+    {
+      sub: id,
+      iat: new Date().getTime(),
+    },
+    process.env.JWT_SECRET
+  );
 };
 
 module.exports = {
   signInApi: (req, res) => {
-    console.log('I AM THE LOGGED IN USER', req.user);
+    console.log("I AM THE LOGGED IN USER", req.user);
     res.json(tokenForUser(req.user.id));
   },
   signUpApi: async (req, res) => {
@@ -23,8 +24,7 @@ module.exports = {
       res.json(tokenForUser(user.id));
     } catch (e) {
       console.log(e);
-      res.status(400)
-        .json(e);
+      res.status(400).json(e);
     }
   },
 };
