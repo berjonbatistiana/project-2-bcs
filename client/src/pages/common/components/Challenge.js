53605427f41870fd5121383ef862278c061d18c6
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-06 10:14:13 -0800

    transfer function from textboxtypedinput.js to challenge.js

--- client/src/pages/common/components/Challenge.js
+++ client/src/pages/common/components/Challenge.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
+import axios from "axios";
 
 class Challenge extends React.Component {
   state = {
     index: 0,
-    word: "Challenge",
+    wordsToBeTyped: "",
     highlightedWord: (
       <div>
         <span style={{ backgroundColor: "grey" }}>C</span>hallenge
@@ -12,17 +13,37 @@ class Challenge extends React.Component {
     ),
   };
 
+  componentDidMount() {
+    this.handleGenerateWords(this.props.wordCount, this.props.minChar).then(res => {
+      this.setState({
+        wordsToBeTyped: res.join(' ')
+      })
+    })
+
+  }
+
+   handleGenerateWords = async (wordCount, minChar) => {
+    try {
+      const params = {params: {wordCount, minChar}};
+      const res = await axios.get('/api/words', params);
+      console.log(res);
+      return res.data;
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+
   handleCorrectKeyDown = (e) => {
-    if (e.key !== this.state.word[this.state.index]) {
+    if (e.key !== this.state.wordsToBeTyped[this.state.index]) {
       return;
     }
     let beginning = "";
     let highlighted = "";
     let end = "";
-    beginning = this.state.word.slice(0, this.state.index + 1);
-    highlighted = this.state.word[this.state.index + 1];
-    if (this.state.index !== this.state.word.length) {
-      end = this.state.word.slice(this.state.index + 2, this.state.word.length);
+    beginning = this.state.wordsToBeTyped.slice(0, this.state.index + 1);
+    highlighted = this.state.wordsToBeTyped[this.state.index + 1];
+    if (this.state.index !== this.state.wordsToBeTyped.length) {
+      end = this.state.wordsToBeTyped.slice(this.state.index + 2, this.state.wordsToBeTyped.length);
     }
     const newHighlightedWord = (
       <div>
