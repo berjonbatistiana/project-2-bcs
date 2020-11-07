22b9d24220b41ea34ed4f1ffe531f84585197f85
Brandon Scott <btfs95@gmail.com>
2020-11-07 12:04:53 -0800

    updated buttons with grid system from material UI rather than boxes

--- client/src/pages/Viewer/ViewerViews/About.js
+++ client/src/pages/Viewer/ViewerViews/About.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Typography, Grid } from "@material-ui/core"
-import Button from '@material-ui/core/Button';
+import { Typography, Grid } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import { ChatContainer, Footer } from "../../common/components";
-
 
 export const About = () => {
   return (
@@ -12,7 +11,7 @@ export const About = () => {
       <Grid
         container
         direction="row"
-        style={{ minHeight: "94vh"}}
+        style={{ minHeight: "94vh" }}
         justify="center"
         alignItems="center"
         xs={12}
@@ -25,12 +24,21 @@ export const About = () => {
               </Typography>
             </Box>
             <Box m={3}>
-              <Typography component="h3" variant="h5" >
+              <Typography component="h3" variant="h5">
                 Learn to type faster with Hype Type today.
               </Typography>
             </Box>
             <Box m={3}>
-              <Button to="signup" component={Link} variant="contained" style={{ color: 'white', backgroundColor: '#0099ff', borderRadius: 25}}>
+              <Button
+                to="signup"
+                component={Link}
+                variant="contained"
+                style={{
+                  color: "white",
+                  backgroundColor: "#0099ff",
+                  borderRadius: 25,
+                }}
+              >
                 Get Started
               </Button>
             </Box>
@@ -38,34 +46,35 @@ export const About = () => {
         </Grid>
         <Grid item xs={10} sm={7} md={5} lg={4}>
           <Box m={3}>
-            <ChatContainer/>
+            <ChatContainer />
           </Box>
         </Grid>
       </Grid>
       <Grid
         container
         direction="row"
-        style={{ minHeight: "50vh", backgroundColor: '#0099ff'}}
+        style={{ minHeight: "50vh", backgroundColor: "#0099ff" }}
         justify="center"
         alignItems="center"
         xs={12}
       >
         <Grid item xs={6}>
-          <Box m={3} style={{color: "white"}}>
+          <Box m={3} style={{ color: "white" }}>
             {/* <Typography component="h3" variant="button" >
               What is Hype Type?
             </Typography> */}
-            <Typography component="h4" variant="h6" >
+            <Typography component="h4" variant="h6">
               Hype Type - The Fastest Way To Test & Increase Your Typing Speed
             </Typography>
-            <Typography component="p" variant="body1" >
+            <Typography component="p" variant="body1">
               <ul>
                 <li>Test your typing speed.</li>
                 <li>Practice lessons to get better.</li>
                 <li>Track your progress.</li>
                 <li>Compare with friends.</li>
               </ul>
-              Hype Type has helped hundreds of individuals improve their typing speeds by over 20 words per minute on average! <br/>
+              Hype Type has helped hundreds of individuals improve their typing
+              speeds by over 20 words per minute on average! <br />
               (Add screenshot on either side)
             </Typography>
           </Box>
@@ -74,7 +83,7 @@ export const About = () => {
       <Grid
         container
         direction="row"
-        style={{ minHeight: "30vh", backgroundColor: '#f5f5f5'}}
+        style={{ minHeight: "30vh", backgroundColor: "#f5f5f5" }}
         justify="center"
         alignItems="center"
         xs={12}
@@ -86,19 +95,47 @@ export const About = () => {
             </Typography>
             <Typography component="p" variant="body2">
               <ul>
-                <li>Test your words per minute speed and accuracy with our typing challenges.</li>
-                <li>Create an account to save your score, challenge friends and track progress!</li>
+                <li>
+                  Test your typing speed with our typing
+                  challenges.
+                </li>
+                <li>
+                  Create an account to save your score, challenge friends and
+                  track progress!
+                </li>
               </ul>
             </Typography>
           </Box>
-          <Box m={1}>
-            <Button to="signup" component={Link} variant="outlined" style={{ color: "#0099ff", borderRadius: 25, borderColor: "#0099ff", marginRight: "15%", marginLeft: 'auto'}}>
-              Typing Challenge
-            </Button>
-            <Button to="signup" component={Link} variant="outlined" style={{ color: "#0099ff", borderRadius: 25, borderColor: "#0099ff"}}>
-              Create an Account
-            </Button>
-          </Box>
+          <Grid container spacing={2}>
+            <Grid item xs>
+              <Button
+                to="signup"
+                component={Link}
+                variant="outlined"
+                style={{
+                  color: "#0099ff",
+                  borderRadius: 25,
+                  borderColor: "#0099ff",
+                }}
+              >
+                Typing Challenge
+              </Button>
+            </Grid>
+            <Grid item xs>
+              <Button
+                to="signup"
+                component={Link}
+                variant="outlined"
+                style={{
+                  color: "#0099ff",
+                  borderRadius: 25,
+                  borderColor: "#0099ff",
+                }}
+              >
+                Create an Account
+              </Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
       <Grid
@@ -111,39 +148,45 @@ export const About = () => {
       >
         <Grid item container xs={10}>
           <Grid item xs={12}>
-            <Box m={3} style={{textAlign: "center"}}>
-              <Typography component="h3" variant="button" >
+            <Box m={3} style={{ textAlign: "center" }}>
+              <Typography component="h3" variant="button">
                 Why Hype Type?
               </Typography>
-              <Typography component="h4" variant="h5" >
+              <Typography component="h4" variant="h5">
                 Reasons to learn how to type now!
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Box m={3} style={{textAlign: "center"}}>
-              <Typography component="p" variant="body1" >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis et fuga necessitatibus, perferendis praesentium quibusdam!
+            <Box m={3} style={{ textAlign: "center" }}>
+              <Typography component="p" variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Blanditiis et fuga necessitatibus, perferendis praesentium
+                quibusdam!
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Box m={3} style={{textAlign: "center"}}>
-              <Typography component="p" variant="body1" >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis et fuga necessitatibus, perferendis praesentium quibusdam!
+            <Box m={3} style={{ textAlign: "center" }}>
+              <Typography component="p" variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Blanditiis et fuga necessitatibus, perferendis praesentium
+                quibusdam!
               </Typography>
             </Box>
           </Grid>
           <Grid item xs={12} md={4}>
-            <Box m={3} style={{textAlign: "center"}}>
-              <Typography component="p" variant="body1" >
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis et fuga necessitatibus, perferendis praesentium quibusdam!
+            <Box m={3} style={{ textAlign: "center" }}>
+              <Typography component="p" variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Blanditiis et fuga necessitatibus, perferendis praesentium
+                quibusdam!
               </Typography>
             </Box>
           </Grid>
         </Grid>
       </Grid>
-      <Footer/>
+      <Footer />
     </React.Fragment>
   );
 };
