cbbb5e0d69385dc181e3b389eeccac2d05f05ea1
smarten <smarten.developer@gmail.com>
2020-11-07 11:26:30 -0800

    format

--- client/src/pages/common/components/Challenge.js
+++ client/src/pages/common/components/Challenge.js
@@ -13,23 +13,26 @@ class Challenge extends React.Component {
     document.addEventListener("keydown", this.handleCorrectKeyDown);
   }
 
-	handleNewChallenge = () => {
-		let options;
-		const {wordCount, minChar, maxChar} = this.props;
-		options = {wordCount, minChar, maxChar};
-			generateWord(options).then(res => {
-				console.log(res)
-				const newWordsToBeTyped = res.join(' ');
-				const beginning = newWordsToBeTyped.slice(0, this.state.index);
-				const highlighted = newWordsToBeTyped[this.state.index];
-				const end = newWordsToBeTyped.slice(this.state.index + 1, newWordsToBeTyped.length);
-			const newHighlightedWord = (
-				<div>
-					{beginning}
-					<span style={{ backgroundColor: "grey" }}>{highlighted}</span>
-					{end}
-				</div>
-			);
+  handleNewChallenge = () => {
+    let options;
+    const { wordCount, minChar, maxChar } = this.props;
+    options = { wordCount, minChar, maxChar };
+    generateWord(options).then((res) => {
+      console.log(res);
+      const newWordsToBeTyped = res.join(" ");
+      const beginning = newWordsToBeTyped.slice(0, this.state.index);
+      const highlighted = newWordsToBeTyped[this.state.index];
+      const end = newWordsToBeTyped.slice(
+        this.state.index + 1,
+        newWordsToBeTyped.length
+      );
+      const newHighlightedWord = (
+        <div>
+          {beginning}
+          <span style={{ backgroundColor: "grey" }}>{highlighted}</span>
+          {end}
+        </div>
+      );
       this.setState({
         wordsToBeTyped: newWordsToBeTyped,
         highlightedWord: newHighlightedWord,
@@ -37,54 +40,57 @@ class Challenge extends React.Component {
     });
   };
 
-	handleCorrectKeyDown = (e) => {
-		if (e.key !== this.state.wordsToBeTyped[this.state.index]) {
-			return;
-		}
-		const beginning = this.state.wordsToBeTyped.slice(0, this.state.index + 1);
-		const highlighted = this.state.wordsToBeTyped[this.state.index + 1];
-		let end = "";
-			
-		if (this.state.index !== this.state.wordsToBeTyped.length) {
-			end = this.state.wordsToBeTyped.slice(this.state.index + 2, this.state.wordsToBeTyped.length);
-				if (this.state.index + 1 === this.state.wordsToBeTyped.length){
-					console.log('refresh')
-					// this.componentDidUpdate() -> could use?
-					this.handleRefreshWords()
-					return
-				}
-			}
-			const newHighlightedWord = (
-				<div>
-					{beginning}
-					<span style={{backgroundColor: "grey"}}>{highlighted}</span>
-					{end}
-				</div>
-			);
-			this.setState({highlightedWord: newHighlightedWord});
-			this.setState({index: this.state.index + 1});
-    };
-
-    // if last item is crossed, run handleNewChallenge
-    handleRefreshWords = () =>{
-        console.log('refresh Conf')
-        // Need to figure out how to set the index back to 0. Maybe a problem with asynchronisity. 
-        this.setState({
-            index: 0,
-            wordsToBeTyped: '',
-            highlightedWord: ''
-        });
-        this.handleNewChallenge()
+  handleCorrectKeyDown = (e) => {
+    if (e.key !== this.state.wordsToBeTyped[this.state.index]) {
+      return;
     }
+    const beginning = this.state.wordsToBeTyped.slice(0, this.state.index + 1);
+    const highlighted = this.state.wordsToBeTyped[this.state.index + 1];
+    let end = "";
 
-    render() {
-        return (
-            <div tabIndex="0" onKeyDown={this.handleCorrectKeyDown}>
-                <h1>Typing Challenge</h1>
-                {this.state.highlightedWord}
-            </div>
-        );
+    if (this.state.index !== this.state.wordsToBeTyped.length) {
+      end = this.state.wordsToBeTyped.slice(
+        this.state.index + 2,
+        this.state.wordsToBeTyped.length
+      );
+      if (this.state.index + 1 === this.state.wordsToBeTyped.length) {
+        console.log("refresh");
+        // this.componentDidUpdate() -> could use?
+        this.handleRefreshWords();
+        return;
+      }
     }
+    const newHighlightedWord = (
+      <div>
+        {beginning}
+        <span style={{ backgroundColor: "grey" }}>{highlighted}</span>
+        {end}
+      </div>
+    );
+    this.setState({ highlightedWord: newHighlightedWord });
+    this.setState({ index: this.state.index + 1 });
+  };
+
+  // if last item is crossed, run handleNewChallenge
+  handleRefreshWords = () => {
+    console.log("refresh Conf");
+    // Need to figure out how to set the index back to 0. Maybe a problem with asynchronisity.
+    this.setState({
+      index: 0,
+      wordsToBeTyped: "",
+      highlightedWord: "",
+    });
+    this.handleNewChallenge();
+  };
+
+  render() {
+    return (
+      <div tabIndex="0" onKeyDown={this.handleCorrectKeyDown}>
+        <h1>Typing Challenge</h1>
+        {this.state.highlightedWord}
+      </div>
+    );
+  }
 }
 
 export default Challenge;
