7ba3879ebb97be5a13b89fdc1cea319dfb1bee1c
Alisa Poon <alisa.poon97@gmail.com>
2020-11-06 21:10:11 -0800

    able to backspace on the challenge component

--- client/src/pages/common/components/Challenge.js
+++ client/src/pages/common/components/Challenge.js
@@ -1,68 +1,90 @@
 import React from "react";
-import {generateWord} from '../../../utils'
+import { generateWord } from "../../../utils";
 
 class Challenge extends React.Component {
     state = {
         index: 0,
-        wordsToBeTyped: '',
-        highlightedWord: ''
+        wordsToBeTyped: "",
+        highlightedWord: "",
     };
 
     componentDidMount() {
         this.handleNewChallenge();
+        document.addEventListener("keydown", this.handleCorrectKeyDown);
     }
 
     handleNewChallenge = () => {
         let options;
-        const {wordCount, minChar, maxChar} = this.props;
-        options = {wordCount, minChar, maxChar};
-        generateWord(options).then(res => {
-            console.log(res)
-            const newWordsToBeTyped = res.join(' ');
+        const { wordCount, minChar, maxChar } = this.props;
+        options = { wordCount, minChar, maxChar };
+        generateWord(options).then((res) => {
+            console.log(res);
+            const newWordsToBeTyped = res.join(" ");
             const beginning = newWordsToBeTyped.slice(0, this.state.index);
             const highlighted = newWordsToBeTyped[this.state.index];
-            const end = newWordsToBeTyped.slice(this.state.index + 1, newWordsToBeTyped.length);
+            const end = newWordsToBeTyped.slice(
+              this.state.index + 1,
+              newWordsToBeTyped.length
+            );
 
             const newHighlightedWord = (
-                <div>
-                    {beginning}
-                    <span style={{backgroundColor: "grey"}}>{highlighted}</span>
-                    {end}
-                </div>
+              <div>
+                  {beginning}
+                  <span style={{ backgroundColor: "grey" }}>{highlighted}</span>
+                  {end}
+              </div>
             );
             this.setState({
                 wordsToBeTyped: newWordsToBeTyped,
-                highlightedWord: newHighlightedWord
-            })
-        })
-    }
+                highlightedWord: newHighlightedWord,
+            });
+        });
+    };
 
     handleCorrectKeyDown = (e) => {
-        if (e.key !== this.state.wordsToBeTyped[this.state.index]) {
-            return;
-        }
-        const beginning = this.state.wordsToBeTyped.slice(0, this.state.index + 1);
-        const highlighted = this.state.wordsToBeTyped[this.state.index + 1];
+        let beginning, highlighted;
         let end = "";
-        if (this.state.index !== this.state.wordsToBeTyped.length) {
-            end = this.state.wordsToBeTyped.slice(this.state.index + 2, this.state.wordsToBeTyped.length);
+        if (e.key === "Backspace") {
+            beginning = this.state.wordsToBeTyped.slice(0, this.state.index - 1);
+            highlighted = this.state.wordsToBeTyped[this.state.index - 1];
+            if (this.state.index !== this.state.wordsToBeTyped.length) {
+                end = this.state.wordsToBeTyped.slice(
+                  this.state.index,
+                  this.state.wordsToBeTyped.length
+                );
+            }
+            this.setState({ index: this.state.index - 1 });
+        } else {
+            beginning = this.state.wordsToBeTyped.slice(0, this.state.index + 1);
+            highlighted = this.state.wordsToBeTyped[this.state.index + 1];
+            if (this.state.index !== this.state.wordsToBeTyped.length) {
+                end = this.state.wordsToBeTyped.slice(
+                  this.state.index + 2,
+                  this.state.wordsToBeTyped.length
+                );
+            }
+            this.setState({ index: this.state.index + 1 });
         }
         const newHighlightedWord = (
-            <div>
-                {beginning}
-                <span style={{backgroundColor: "grey"}}>{highlighted}</span>
-                {end}
-            </div>
+          <div>
+              {beginning}
+              <span style={{ backgroundColor: "grey" }}>{highlighted}</span>
+              {end}
+          </div>
         );
-        this.setState({highlightedWord: newHighlightedWord});
-        this.setState({index: this.state.index + 1});
+        this.setState({ highlightedWord: newHighlightedWord });
     };
 
     render() {
+        const style = {
+            fontSize: 30,
+            textAlign: "center",
+        };
         return (
-            <div tabIndex="0" onKeyDown={this.handleCorrectKeyDown}>
-                {this.state.highlightedWord}
-            </div>
+          <div style={style}>
+              <h1>Typing Challenge</h1>
+              {this.state.highlightedWord}
+          </div>
         );
     }
 }
