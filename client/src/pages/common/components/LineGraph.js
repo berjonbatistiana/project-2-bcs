66d3fd972a099bf3f488042ad1775dd2af539956
smarten <smarten.developer@gmail.com>
2020-11-09 20:51:05 -0800

    nit

--- client/src/pages/common/components/LineGraph.js
+++ client/src/pages/common/components/LineGraph.js
@@ -6,16 +6,13 @@ export class LineGraph extends Component {
 
   componentDidMount() {
     const myChartRef = this.chartRef.current.getContext("2d");
-    const ids = [];
     const wpm = [];
-    for (let i = 0; i < this.props.userData.length; i++) {
-      ids.push(i + 1);
-      wpm.push(this.props.userData[i].wordsPerMin);
-    }
+    this.props.userData.forEach(item => {
+      wpm.push(item.wordsPerMin);
+    })
     new Chart(myChartRef, {
       type: "line",
       data: {
-        labels: [...ids].reverse(),
         datasets: [
           {
             label: "WPM",
