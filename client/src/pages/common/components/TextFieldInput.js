88218431c00488be700419831ceeedc5a3604a5f
Alisa Poon <alisa.poon97@gmail.com>
2020-11-11 19:51:16 -0800

    must be logged in to view challenge, password is hidden, and text input is light blue

--- client/src/pages/common/components/TextFieldInput.js
+++ client/src/pages/common/components/TextFieldInput.js
@@ -1,13 +1,27 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-import {accentColor} from "../components"
+import React from 'react';
+import {
+  ThemeProvider,
+  makeStyles,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { lightBlue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: lightBlue,
+  },
+});
 
 export const TextFieldInput = ({ input, label }) => {
   return (
+    <ThemeProvider theme={theme}>
       <TextField
         {...input}
         label={label}
         fullWidth
+        type={label === "Password" ? "password" : "text"}
       />
+    </ThemeProvider>
     );
 };
