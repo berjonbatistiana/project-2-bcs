5e3c5f413682b76e4d6f6a6a329e36248dd61fad
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-14 12:12:36 -0800

    hash router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
-const path = require("path");
+// const path = require("path");
 
 require("./services/passport");
 
@@ -16,10 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build"));
-
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build"));
+// });
 
 app.listen(PORT, () => {
   console.log("Server started listening on PORT http://localhost:3001");
