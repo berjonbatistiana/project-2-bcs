25579d8102340d1d76ec30336836d808ce3c2d19
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-14 12:01:54 -0800

    manny suggestion 3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build"));
 
   });
 }
