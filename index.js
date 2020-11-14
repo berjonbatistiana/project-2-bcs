a0e89d629bfabfe27da8da2768c7afec0621995e
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-14 12:06:42 -0800

    manny suggestion 4

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build"));
-
-  });
 }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build"));
+
+});
 
 app.listen(PORT, () => {
   console.log("Server started listening on PORT http://localhost:3001");
