18b6e51287e581f774df993088fadcc2216c2d78
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-14 12:31:20 -0800

    Using HashRouter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
-const path = require("path");
+// const path = require("path");
 
 require("./services/passport");
 
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+  // app.get("/*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  // });
 }
 
 app.use(express.urlencoded({ extended: true }));
