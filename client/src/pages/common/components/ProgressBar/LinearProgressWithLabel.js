cc2d8bfd2f1c5ae71f53c916b89024ab4af13e11
Bermond Jon Batistiana <berjondev@gmail.com>
2020-11-17 13:20:02 -0800

    Styled progressbar

--- client/src/pages/common/components/ProgressBar/LinearProgressWithLabel.js
+++ client/src/pages/common/components/ProgressBar/LinearProgressWithLabel.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import {accentColor} from "../accentColor";
+import {accentColor, secondaryColor} from "../accentColor";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 function LinearProgressWithLabel(props) {
+
+  const StyledLinearProgress = withStyles({
+    colorPrimary: {
+      backgroundColor: secondaryColor
+    },
+    barColorPrimary: {
+      backgroundColor: accentColor
+    }
+  })(LinearProgress);
+
   return (
     <Box display="flex" alignItems="center">
       <Box width="100%" mr={1}>
-        <LinearProgress variant="determinate" {...props} />
+        <StyledLinearProgress variant="determinate" {...props} />
       </Box>
       <Box minWidth={35}>
         <Typography variant="body2" color="textSecondary">{`${Math.round(
@@ -31,9 +42,8 @@ LinearProgressWithLabel.propTypes = {
 
 const useStyles = makeStyles({
   root: {
-    color: accentColor,
     width: '100%',
-  },
+  }
 });
 
 export default function LinearWithValueLabel(props) {
@@ -41,7 +51,7 @@ export default function LinearWithValueLabel(props) {
   console.log(props.progress);
   return (
     <div className={classes.root}>
-      <LinearProgressWithLabel value={props.progress} />
+      <LinearProgressWithLabel value={props.progress}/>
     </div>
   );
 }
